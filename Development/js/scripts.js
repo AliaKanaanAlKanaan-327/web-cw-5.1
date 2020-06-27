a97f508dcfee55de73dc4e809c8215ab505f0a60
AliaKanaanAlKanaan-327 <aliakanan444@gmail.com>
2020-06-27 21:54:00 +0300

    JavaScript 1

--- Development/js/scripts.js
+++ Development/js/scripts.js
@@ -1,34 +1,76 @@
 // السؤال الأول
 // 1
+const name = "عالية";
 
 // 2
+let age = 16;
 
 // 3 and 4
+// console.log("اسمي",+ name, + "و عمري", + age);
+console.log("أنا "+ name + " " + "و عمري "+ age)
 
 // 5 and 6
+let five_times_age = age*5;
+console.log(five_times_age)
 
+console.log(age*5)
+
+// age *=5
+console.log(age)
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // السؤال الثاني
 // 1
+let hobbies = ["الرسم",
+"القراءة",
+"السباحة"]
+
 
 // 2
+hobbies.push(age)
+
+hobbies[4] = 16
+console.log(hobbies)
+
+console.log(hobbies.length)
 
 // 3
+hobbies.pop()
 
+console.log(hobbies)
 // 4
+let student = {
+    track: "مسار الويب",
+    language: "JavaScript",
+}
 
 // 5
+student["TAs"] = ["نانسي",
+"موضي",
+"حسين",
+"رهف"]
+console.log(student)
+
 
 // 6
+console.log(student["track"])
+console.log(student.track)
 
 // 7
+console.log(student["TAs"][0])
 
 // -.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-
 
 // البونص
 // 1
+student["SayHello"] = function() {
+    console.log("مرحبا")
+}
+
+
+console.log(student)
 
 // 2
+student["SayHello"]()
 
 // console results => console.png
